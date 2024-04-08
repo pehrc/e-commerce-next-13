305f81c3ba8a412e84355c5d387cc895d956f397
pcorreia <pedro.correia@allcomsistemas.com>
2024-04-08 10:22:02 -0300

    :sparkles: componente header

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+
+import Header from '../components/header';
+
+type StoreLayoutType = { children: ReactNode };
+
+export default function StoreLayout({ children }: StoreLayoutType) {
+  return (
+    <div className="mx-auto grid min-h-screen w-full max-w-[1440px] grid-rows-[min-content_max-content] gap-5 p-8">
+      <Header />
+      {children}
+    </div>
+  );
+}
