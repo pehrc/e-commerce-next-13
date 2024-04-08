9ddeb390de8698b1ffaae11057a0aab2032b5938
pcorreia <pedro.correia@allcomsistemas.com>
2024-04-08 14:23:54 -0300

    :sparkles: Fetch de produtos na Home

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,6 @@
 import data from '../data.json';
 
-export default function GET() {
+export function GET() {
   const featuredProducts = data.products.filter(product => product.featured);
 
   return Response.json(featuredProducts);
