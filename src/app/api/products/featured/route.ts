2020aa4a9d9043b84fa7872c5e9a482067d766cc
pcorreia <pedro.correia@allcomsistemas.com>
2024-04-10 08:53:13 -0300

    :boom: Loading na Home

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,8 @@
 import data from '../data.json';
 
-export function GET() {
+export async function GET() {
+  await new Promise(resolve => setTimeout(resolve, 5000));
+
   const featuredProducts = data.products.filter(product => product.featured);
 
   return Response.json(featuredProducts);
